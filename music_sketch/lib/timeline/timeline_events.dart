ea6ea5eef91797e595041485b1bb45cb90dd03a2
けもみみ愛してる <hirohiro_j3cub@yahoo.co.jp>
2021-08-04 17:49:27 +0900

    タイムラインのスクロール機能の追加とバグの修正

--- music_sketch/lib/timeline/timeline_events.dart
+++ music_sketch/lib/timeline/timeline_events.dart
@@ -2,29 +2,38 @@ import 'package:flutter/material.dart';
 import 'timeline_data.dart';
 import 'timeline_track.dart';
 
-class TimelineEvents<T> extends StatelessWidget  implements  TimelineDataFactry<T> {
+class TimelineEvents<T> extends StatelessWidget
+    implements TimelineDataFactry<T> {
   late final List<TimelineTrack<T>> lines;
 
   TimelineEvents.test(int lineCount, {Key? key}) : super(key: key) {
-    lines =
-        List<TimelineTrack<T>>.generate(lineCount, (i) => TimelineTrack<T>.test());
+    lines = List<TimelineTrack<T>>.generate(
+        lineCount, (i) => TimelineTrack<T>.test());
   }
 
   TimelineEvents.empty(int lineCount, {Key? key}) : super(key: key) {
-    lines =
-        List<TimelineTrack<T>>.generate(lineCount, (i) => TimelineTrack<T>.empty());
+    lines = List<TimelineTrack<T>>.generate(
+        lineCount, (i) => TimelineTrack<T>.empty());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: lines,
-    );
+    final _scrollController = ScrollController();
+    return FractionallySizedBox(
+        widthFactor: 1.0,
+        child: Scrollbar(
+            controller: _scrollController,
+            child: SingleChildScrollView(
+                controller: _scrollController,
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: lines))));
   }
 
   @override
   List<List<TimelineElementData<T>>> getDatas() {
-    var d = lines.map((e)=> e.getDatas()[0]).toList();
+    var d = lines.map((e) => e.getDatas()[0]).toList();
     return d;
   }
 }
