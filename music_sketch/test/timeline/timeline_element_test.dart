eb514fbbaf064a0ba21203f1d1e206aa1f1cae03
けもみみ愛してる <hirohiro_j3cub@yahoo.co.jp>
2021-08-07 22:48:12 +0900

    Elementの仕様変更に対応

--- music_sketch/test/timeline/timeline_element_test.dart
+++ music_sketch/test/timeline/timeline_element_test.dart
@@ -6,7 +6,7 @@ import 'package:music_sketch/timeline/timeline_times.dart';
 void main() {
   var start = TimelinePosition.fromPosition(3);
   var end = TimelinePosition.fromPosition(6);
-  var element = TimelineElement<int>(TimelinePositionRange(start, end), null);
+  var element = TimelineElement<int>(positionRange : TimelinePositionRange(start, end));
   testWidgets('timeline_element_widget', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(home: element));
